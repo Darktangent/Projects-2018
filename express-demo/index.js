1ea4b6a6561bf803cd46b30c7e76cfca87d1f440
Ron Ganguly <c1ph3r9@gmail.com>
2018-10-25 03:31:18 -0500

    uploading index file with updated put and post route handeler

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,11 +1,14 @@
 const express = require('express')
-const app=express()
 const Joi= require('joi')
+const app=express()
 
+// -4 test
 app.use(express.json())
+
 const courses=[
 {id:1, name:"courses 1"},
-{id:2, name:'courses 2'}
+{id:2, name:'courses 2'},
+{id:3,name:'courses 3 '}
 ]
 app.get('/',(req,res)=>{
 	res.send('Hello there!!!')
@@ -22,13 +25,7 @@ app.get('/api/courses/:id',(req,res)=>{
 })
 app.post('/api/courses',(req,res)=>{
 
-	const schema={
-		name:Joi.string().min(3).require()
-
-	}
-	const result=Joi.validate(req.body,schema)
-	
-
+	const result=validateCourse(req.body)
 	if(result.error){
 		res.status(400).send(result.error.details[0].message)
 		return
@@ -41,12 +38,37 @@ app.post('/api/courses',(req,res)=>{
 	res.send(course)
 
 })
+app.put('/api/courses/:id',(req,res)=>{
 
+	//lookup the courses
+	//if doesnt exist, return 404
+	//validate
+	//if invalid. return 400- bad request
+	const course=courses.find(c=>c.id===parseInt(req.params.id))
+	if (!course) res.status(404).send('not found')
 
+	const result=validateCourse(req.body)
+	if(result.error){
+		res.status(400).send(result.error.details[0].message)
+		return
+	}
+	//update courses
+course.name=req.body.name
+	//return updated course
+	res.send(course)
+
+})
+function validateCourse(course){
+
+	const schema={
+		name:Joi.string().min(3).required()
+
+	}
+	return Joi.validate(course,schema)
+}
 
 //PORT
 const port=process.env.PORT || 3000;
 app.listen(port,()=>
 	console.log(`Listening on ${port}`)
 )
-
